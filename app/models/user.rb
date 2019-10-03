d1c8c6112a2d13274a9e041eb2d26dc27648169e
Vadim <kuzminvn2010@yandex.ru>
2019-10-04 00:00:01 +0400

    Create models and password authentification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,37 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  INTEGRATION = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+
+  attr_accessor :password
+
+  #validates_presence_of :password, on: :create
+  #validates_confirmation_of :password
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true
+
+  before_save :encript_password
+
+  def encript_password
+    if self.password.present?
+      # создаем рандомную строку
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      # создаем длинную уникальную строку (хэш)
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pdkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+        )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
 end
