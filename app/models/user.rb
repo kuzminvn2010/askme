2d964299f899265c90f533a85c30e8c44a9d3b20
Vadim <kuzminvn2010@yandex.ru>
2019-10-04 23:49:13 +0400

    Create custom validator and simple validates in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,32 @@
 require 'openssl'
 
 class User < ApplicationRecord
-  INTEGRATION = 20000
+  ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
-
-  has_many :questions
-
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-
 
   attr_accessor :password
 
-  #validates_presence_of :password, on: :create
-  #validates_confirmation_of :password
+  has_many :questions
+
+  validates :email, email: true
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+  validates :username, length: {maximum: 40}, format: {with: /[a-zA-z0-9_]/}
+
+
   validates :password, presence: true, on: :create
   validates :password, confirmation: true
 
   before_save :encript_password
+  before_validation :username_downcase
 
   def encript_password
     if self.password.present?
-      # создаем рандомную строку
+      # создаем рандомную строку, с помощью которой будет шифроваться пароль
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
       # создаем длинную уникальную строку (хэш)
       self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pdkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
         )
     end
   end
@@ -34,4 +35,7 @@ class User < ApplicationRecord
     password_hash.unpack('H*')[0]
   end
 
+  def username_downcase
+    self.username.downcase!
+  end
 end
