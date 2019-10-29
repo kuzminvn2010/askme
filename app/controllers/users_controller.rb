7f198d1925c26465d0405a71d0e7448f00599dc2
Vadim <kuzminvn2010@yandex.ru>
2019-10-29 23:35:37 +0400

    Modify users controller and users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.new(
+      name: 'Vadim',
+      username: 'instalero',
+      avatar_url: 'https://s.gravatar.com/avatar/421c1bd346e2e9d881f864d1ed66d7a6?s=80'
+      )
   end
 end
